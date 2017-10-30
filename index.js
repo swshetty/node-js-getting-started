1165bdc537482e7cc98aeac7eea71b28f3bc4074
Swastik Shetty <swshetty@deloitte.com>
2017-10-30 19:03:23 +0530

    - test changes for new response to fb webhook

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  //response.render('pages/index');
+  response.status(200).send('just some test response');
 });
 
 app.listen(app.get('port'), function() {
